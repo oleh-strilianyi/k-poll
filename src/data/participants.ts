043a34e4b2c9546ab524cd049722745ae2c0a630
ostrilianyi <strelyanyi.oleg@gmail.com>
2025-11-13 13:06:43 +0200

    Remove images from source files

--- src/data/participants.ts
+++ src/data/participants.ts
@@ -1,24 +1,20 @@
 import type { Participant } from '../types';
 
-// Ця змінна автоматично стане '/k-poll/' на GitHub Pages
-// і '/' під час локальної розробки (npm run dev)
-const BASE_URL = import.meta.env.BASE_URL;
-
 export const participants: Participant[] = [
-  { id: 1, name: 'Яна учілка', imageUrl: `${BASE_URL}images/1.jpg` },
+  { id: 1, name: 'Яна учілка', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030486/1_fn3k1j.jpg` },
   // { id: 2, name: 'Софія турнікет', imageUrl: `${BASE_URL}images/2.jpg` },
-  { id: 3, name: 'Настя новая', imageUrl: `${BASE_URL}images/3.jpg` },
+  { id: 3, name: 'Настя новая', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030486/3_i9qqml.jpg` },
   // { id: 4, name: 'Віка вставна челюсть', imageUrl: `${BASE_URL}images/4.jpg` },
-  { id: 5, name: 'Надін у апарата', imageUrl: `${BASE_URL}images/5.jpg` },
-  { id: 6, name: 'Оксана косметолог', imageUrl: `${BASE_URL}images/6.jpg` },
-  { id: 7, name: 'Юля письмо пізніше', imageUrl: `${BASE_URL}images/7.jpg` },
-  { id: 8, name: 'Надя табуретка', imageUrl: `${BASE_URL}images/8.jpg` },
-  { id: 9, name: 'Даша весільне агенство', imageUrl: `${BASE_URL}images/9.jpg` },
-  { id: 10, name: 'Валерія наєздніца', imageUrl: `${BASE_URL}images/10.jpg` },
+  { id: 5, name: 'Надін у апарата', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030486/5_qq52y1.jpg` },
+  { id: 6, name: 'Оксана косметолог', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/6_qjiiym.jpg` },
+  { id: 7, name: 'Юля письмо пізніше', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/7_uce8yc.jpg` },
+  { id: 8, name: 'Надя табуретка', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/8_r461vi.jpg` },
+  { id: 9, name: 'Даша весільне агенство', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/9_cg33ll.jpg` },
+  { id: 10, name: 'Валерія наєздніца', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/10_zrcem1.jpg` },
   // { id: 11, name: 'Ніколетта тату', imageUrl: `${BASE_URL}images/11.jpg` },
-  { id: 12, name: 'Іра купальники', imageUrl: `${BASE_URL}images/12.jpg` },
-  { id: 13, name: 'Ольга фітнес тренер', imageUrl: `${BASE_URL}images/13.jpg` },
-  { id: 14, name: 'Юля фешн фотограф', imageUrl: `${BASE_URL}images/14.jpg` },
-  { id: 15, name: 'Іра шапка з аврори', imageUrl: `${BASE_URL}images/15.jpg` },
-  { id: 16, name: 'Діана Зотова', imageUrl: `${BASE_URL}images/16.jpg` },
+  { id: 12, name: 'Іра купальники', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030484/12_mmayyu.jpg` },
+  { id: 13, name: 'Ольга фітнес тренер', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030485/13_yoonn7.jpg` },
+  { id: 14, name: 'Юля фешн фотограф', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030485/14_qfuw3j.jpg` },
+  { id: 15, name: 'Іра шапка з аврори', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030485/15_ofhelu.jpg` },
+  { id: 16, name: 'Діана Зотова', imageUrl: `https://res.cloudinary.com/dn9a686um/image/upload/v1763030485/16_gsgq2m.jpg` },
 ];
