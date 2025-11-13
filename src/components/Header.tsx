5398fc047bff7c7507a1383eb883b5332935a9c2
ostrilianyi <strelyanyi.oleg@gmail.com>
2025-11-13 13:13:43 +0200

    Adds update message

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Точно не Холостяк 2025</h1>
+      <h1>Точно не Холостяк 2025 (Триває оновлення)</h1>
     </header>
   );
 }
