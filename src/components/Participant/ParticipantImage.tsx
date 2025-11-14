516f76495937f4637c0f87f1f6c5027b688948a7
ostrilianyi <strelyanyi.oleg@gmail.com>
2025-11-14 18:05:09 +0200

    Fix deploy

--- src/components/Participant/ParticipantImage.tsx
+++ src/components/Participant/ParticipantImage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import type { ParticipantPhoto } from '../types';
+import type { ParticipantPhoto } from '../../types';
 import styles from './ParticipantImage.module.css';
 
 interface ParticipantImageProps {
