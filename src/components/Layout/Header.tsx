d4f6359539a631a81defd7b5c1cfa4ac1c3ecd77
ostrilianyi <strelyanyi.oleg@gmail.com>
2025-12-05 17:25:20 +0200

    new week

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Тиждень 7</h1>
+      <h1>Тиждень 8</h1>
     </header>
   );
 }
